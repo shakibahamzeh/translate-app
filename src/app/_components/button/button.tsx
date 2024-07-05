d26b12a0f8e02e4b3899cd7c5e106969cf98ff27
shakiba Hamzeh <shakiba.h_70@yahoo.com>
2024-07-05 22:45:28 +0430

    create button component

---
+++ src/app/_components/button/button.tsx
@@ -0,0 +1,43 @@
+// components/Button.tsx
+
+import React from 'react';
+
+type ButtonProps = {
+  onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  type?: 'button' | 'submit' | 'reset';
+  children: React.ReactNode;
+  variant?: 'primary' | 'secondary' | 'danger';
+  disabled?: boolean;
+  className?: string;
+};
+
+const Button: React.FC<ButtonProps> = ({
+  onClick,
+  type = 'button',
+  children,
+  variant = 'primary',
+  disabled = false,
+  className = '',
+}) => {
+  const baseClasses = 'px-4 py-2 font-semibold rounded-md';
+  const variantClasses = {
+    primary: 'bg-blue-500 text-white hover:bg-blue-600',
+    secondary: 'bg-gray-500 text-white hover:bg-gray-600',
+    danger: 'bg-red-500 text-white hover:bg-red-600',
+  };
+
+  const combinedClasses = `${baseClasses} ${variantClasses[variant]} ${className} ${disabled ? 'opacity-50 cursor-not-allowed' : ''}`;
+
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={combinedClasses}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;

ea5bda1deb2eeaf325f9cd32d6fee42945de88fd
shakiba Hamzeh <shakiba.h_70@yahoo.com>
2024-07-05 22:45:16 +0430

    create textarea component

---
+++ src/app/_components/textarea/textarea.tsx
@@ -0,0 +1,57 @@
+// components/Textarea.tsx
+
+import React from 'react';
+
+type TextareaProps = {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  placeholder?: string;
+  rows?: number;
+  cols?: number;
+  maxLength?:number;
+  length?:number;
+  disabled?: boolean;
+  style?: React.CSSProperties;
+  className?: string;
+};
+
+const Textarea: React.FC<TextareaProps> = ({
+  value,
+  onChange,
+  length,
+  maxLength,
+  placeholder = '',
+  rows = 5,
+  cols = 33,
+  disabled = false,
+  style,
+  className,
+}) => {
+  return (
+    <div>
+    <textarea
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+      rows={rows}
+      cols={cols}
+      disabled={disabled}
+      style={style}
+      className={className}
+    />
+    <p className='text-[#4D5562] flex w-full justify-end text-xs font-semibold'>{length}/{maxLength}</p>
+    </div>
+  );
+};
+
+export default Textarea;
+
+
+// fetch('https://api.mymemory.translated.net/get?q=Hello,%20how%20are%20you?!&langpair=en|fr')
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data)
+//   })
+//   .catch(error => {
+//     console.log(error)
+//   })

069a090cda4042d5ca7c387fcdf9360b5011fa1c
shakiba Hamzeh <shakiba.h_70@yahoo.com>
2024-07-05 22:46:44 +0430

    completing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Card from "./_components/translateCard/card";
 
 export default function Home() {
   return (
-    <main className="bg-[url('/images/hero_img.jpg')]  w-full h-screen bg-no-repeat bg-cover">
-      بقیش با تو!
+    <main className="bg-[url('/images/hero_img.jpg')]  w-full h-screen bg-no-repeat bg-cover grid grid-cols-2 gap-x-8 m-auto p-12 items-center">
+      <Card/>
+      {/* <Card/> */}
     </main>
   )
 }

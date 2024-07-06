da7fa9188894831d0f428651f678b14d54fc6151
shakiba <shakiba.hamze70@gmail.com>
2024-07-06 12:03:38 +0330

    wrapping QueryProvider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
+"use client"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Card from "./_components/translateCard/card";
+import AnswerCard from "./_components/answerCard/answerCard";
+
+const queryClient = new QueryClient();
 
 export default function Home() {
+
   return (
+    <QueryClientProvider client={queryClient}>
     <main className="bg-[url('/images/hero_img.jpg')]  w-full h-screen bg-no-repeat bg-cover grid grid-cols-2 gap-x-8 m-auto p-12 items-center">
       <Card/>
-      {/* <Card/> */}
+      <AnswerCard/>
     </main>
+    </QueryClientProvider>
   )
 }

a5a292f67776c2d916d860a25eecbcbc0dbccec7
Shakiba Hamzeh <S.Hamzeh@nitro.local>
2025-04-16 15:01:36 +0330

    add copy and listen to text options and active buttons

--- src/app/_components/answerCard/answerCard.tsx
+++ src/app/_components/answerCard/answerCard.tsx
@@ -35,22 +35,52 @@ const AnswerCard = () => {
             });
         }
       };
+
+      const handleSpeak = () => {
+        if (!answer) return;
+      
+        const utterance = new SpeechSynthesisUtterance(answer);
+        const voices = window.speechSynthesis.getVoices();
+        const voice = voices.find(v => v.lang.startsWith(toLanguage)); // مثلاً 'fr' یا 'en'
+      
+        if (voice) utterance.voice = voice;
+        utterance.lang = toLanguage;
+        window.speechSynthesis.speak(utterance);
+      };
+      
+      
   return (
-    <div className='h-96 bg-[#040711] rounded-2xl p-8'>
+    <div className='h-96 bg-[#040711] rounded-2xl p-8 lg:w-[500px]'>
         <header className='text-[#4D5562] flex pb-4 border-b border-[#4D5562] gap-x-4 font-semibold'>
-             {
-                languages.map(lang => <li key={lang.id} className='list-none text-sm cursor-pointer' onClick={()=>setToLanguage(lang.keyLang)}>{lang.lang}</li>)
-             }
+          {languages.map(lang => (
+            <li
+              key={lang.id}
+              className={`list-none text-sm cursor-pointer px-2 py-1 rounded-md transition-all duration-200
+                ${toLanguage === lang.keyLang ? 'bg-red-500 text-white' : ''}`}
+              onClick={() => setToLanguage(lang.keyLang)}
+            >
+              {lang.lang}
+            </li>
+          ))}
         </header>
         <p className='min-h-[200px] pt-6 text-white'>{answer}</p>
         <div className='w-full flex justify-between mt-10 items-center'>
             <div className='flex gap-x-4'>
-                <div className='p-1 border border-[#4D5562] flex items-center justify-center h-8 w-8 rounded-lg cursor-pointer'>
-                    <VolumeHigh size="18" color="#4D5562"/>
-                </div>
-                <div className='p-1 border border-[#4D5562] flex items-center justify-center h-8 w-8 rounded-lg cursor-pointer'>
-                    <Copy size="18" color="#4D5562" onClick={handleCopyToClipboard}/>
-                </div>
+            <div
+              className='p-1 border border-[#4D5562] flex items-center justify-center h-8 w-8 rounded-lg cursor-pointer'
+              onClick={handleSpeak}
+            >
+              <VolumeHigh size="18" color="#4D5562" />
+            </div>
+
+                  <div
+                    className='p-1 border border-[#4D5562] flex items-center justify-center h-8 w-8 rounded-lg cursor-pointer'
+                    onClick={() => {
+                    navigator.clipboard.writeText(answer);
+                    }}
+                  >
+                    <Copy size="18" color="#4D5562" />
+                  </div>
             </div>
         </div>
     </div>
--- src/app/_components/translateCard/card.tsx
+++ src/app/_components/translateCard/card.tsx
@@ -1,86 +1,113 @@
 "use client"
-import React, { useState } from 'react'
+import React from 'react'
 import Textarea from '../textarea/textarea'
 import Button from '../button/button'
 import { Copy, VolumeHigh } from 'iconsax-react'
-import { useQuery } from '@tanstack/react-query'
-import Translate from '@/core/translate.services';
-import {Language} from "./card.types"
+import { useMutation } from '@tanstack/react-query'
+import Translate from '@/core/translate.services'
+import { Language } from "./card.types"
 import useStore from '@/store/useStore'
-import { StoreState } from '@/types/store.types'
-import { useRouter, useSearchParams } from 'next/navigation'
 
 const Card: React.FC = () => {
+  const languages: Language[] = [
+    { id: 0, lang: "Detect Language", keyLang: "autodetect"},
+    { id: 1, lang: "English", keyLang: "en" },
+    { id: 2, lang: "French", keyLang: "fr" },
+    { id: 3, lang: "Spanish", keyLang: "es" }
+  ];
 
-    const languages: Language[] = [
-        {id : 1 , lang : "English" , keyLang : "en"},
-        {id : 2 , lang : "French" , keyLang : "fr"},
-        {id : 3 , lang : "Spanish" , keyLang : "es"}
-    ]
-    const { language, setLanguage, text , setText,toLanguage,setToLanguage,answer, setAnswer} = useStore((state) => ({
-        language: state.language,
-        setLanguage: state.setLanguage,
-        toLanguage: state.toLanguage,
-        setToLanguage: state.setToLanguage,
-        text : state.text,
-        setText : state.setText,
-        answer : state.answer,
-        setAnswer : state.setAnswer
-      }));
+  const {
+    language, setLanguage,
+    toLanguage, setToLanguage,
+    text, setText,
+    answer, setAnswer
+  } = useStore((state) => ({
+    language: state.language,
+    setLanguage: state.setLanguage,
+    toLanguage: state.toLanguage,
+    setToLanguage: state.setToLanguage,
+    text: state.text,
+    setText: state.setText,
+    answer: state.answer,
+    setAnswer: state.setAnswer
+  }));
 
-      const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-            if(text.length < 500){
-                setText(e.target.value);
-            }
-        };
-        console.log(text);
-        
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    if (text.length < 500) {
+      setText(e.target.value);
+    }
+  };
 
-        const { data , refetch} = useQuery({ queryKey: ['getTranslate',text], queryFn: async () => await new Translate().getTranslate({params : { q : text, langpair: `${language}|${toLanguage}`}})});
-        const router = useRouter();
-        console.log(router);
-        
-        const handleSubmit = () => {
-            try {
-                refetch();
-                setAnswer(data?.data?.responseData?.translatedText);
-                // router.push(`/?q=${text}!&langpair=${language}|${toLanguage}?ans=${answer}`)
-            } catch(err){
-                    console.error("Translation failed: ", err);
-            }
-           
-        }
+  const { mutate, isPending } = useMutation({
+    mutationFn: async () => {
+      const res = await new Translate().getTranslate({
+        params: { q: text, langpair: `${language}|${toLanguage}` }
+      });
+      return res?.data?.responseData?.translatedText;
+    },
+    onSuccess: (translatedText: string) => {
+      setAnswer(translatedText || "");
+    },
+    onError: (err: any) => {
+      console.error("Translation failed:", err);
+    }
+  });
 
-        
+  const handleSpeak = () => {
+    if (!text) return;
+  
+    const utterance = new SpeechSynthesisUtterance(text);
+    const voices = window.speechSynthesis.getVoices();
+    const voice = voices.find(v => v.lang.startsWith(language));
+  
+    if (voice) utterance.voice = voice;
+    utterance.lang = language;
+    window.speechSynthesis.speak(utterance);
+  };
+  
   return (
-    <div className='h-96 bg-[#040711] rounded-2xl p-8'>
-        <header className='text-[#4D5562] flex pb-4 border-b border-[#4D5562] gap-x-4 font-semibold'>
-            <li className='list-none text-sm'>Detect Language</li>
-            {
-                languages.map(lang => <li key={lang.id} className='list-none text-sm cursor-pointer' onClick={()=>setLanguage(lang.keyLang)}>{lang.lang}</li>)
-            }
-        </header>
-        <Textarea  
+    <div className='h-96 bg-[#040711] rounded-2xl p-8 lg:w-[500px]'>
+      <header className='text-[#4D5562] flex pb-4 border-b border-[#4D5562] gap-x-4 font-semibold'>
+        {languages.map(lang => (
+          <li
+            key={lang.id}
+            className={`list-none text-sm cursor-pointer px-2 py-1 rounded-md transition-all duration-200
+                ${language === lang.keyLang ? 'bg-red-500 text-white' : ''}`}
+            onClick={() => setLanguage(lang.keyLang)}
+          >
+            {lang.lang}
+          </li>
+        ))}
+      </header>
+
+      <Textarea
         value={text}
         onChange={handleChange}
         className='w-full pt-6 text-sm resize-none min-h-[200px] bg-transparent text-white placeholder:text-[white] outline-none'
         maxLength={500}
         length={text.length}
-        />
-        <div className='w-full flex justify-between mt-4 items-center'>
-            <div className='flex gap-x-4'>
-                <div className='p-1 border border-[#4D5562] flex items-center justify-center h-8 w-8 rounded-lg cursor-pointer'>
-                    <VolumeHigh size="18" color="#4D5562"/>
-                </div>
-                <div className='p-1 border border-[#4D5562] flex items-center justify-center h-8 w-8 rounded-lg cursor-pointer'>
-                    <Copy size="18" color="#4D5562" />
-                </div>
-                
-            </div>
-            <Button onClick={handleSubmit} variant="primary">Translate</Button>
+      />
+
+      <div className='w-full flex justify-between mt-4 items-center'>
+        <div className='flex gap-x-4'>
+          <div className='p-1 border border-[#4D5562] flex items-center justify-center h-8 w-8 rounded-lg cursor-pointer'     onClick={handleSpeak}>
+            <VolumeHigh size="18" color="#4D5562" />
+          </div>
+          <div
+            className='p-1 border border-[#4D5562] flex items-center justify-center h-8 w-8 rounded-lg cursor-pointer'
+            onClick={() => {
+             navigator.clipboard.writeText(text);
+            }}
+          >
+            <Copy size="18" color="#4D5562" />
+          </div>
         </div>
+        <Button onClick={() => mutate()} variant="primary">
+          {isPending ? "Translating..." : "Translate"}
+        </Button>
+      </div>
     </div>
   )
 }
 
-export default Card
+export default Card;

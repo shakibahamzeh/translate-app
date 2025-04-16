65fb308f4a59b8e7686c5f128b14ecb482948474
Shakiba Hamzeh <S.Hamzeh@nitro.local>
2025-04-16 15:03:11 +0330

    improve function

--- src/core/translate.services.ts
+++ src/core/translate.services.ts
@@ -1,30 +1,23 @@
-// <import handler
 import requestModel from "./apiHandlers";
-// import handler>
-
-// <import types
 import { ReqApi, Request } from "../types/services.types";
-// import types>
 
 class Translate {
+  url: string;
 
-    url: string;
+  constructor() {
+    this.url = "get";
+  }
 
-    constructor() {
-        this.url = "get";
-    }
-
-    async getTranslate({params} : ReqApi) {
-
-        const apiObj : Request = {
-            url : this.url ,
-            params
-        };
-
-        return await requestModel.response(apiObj);
+  async getTranslate({ params }: ReqApi) {
+    const apiObj: Request = {
+      url: this.url,
+      method: "get", 
+      params,       
+      contentType: "application/json"
     };
 
-
-};
+    return await requestModel.response(apiObj);
+  }
+}
 
 export default Translate;

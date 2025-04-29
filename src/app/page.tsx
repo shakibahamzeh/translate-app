fffb24bbcdebbf22c0e563d16ac646705224b94c
Shakiba Hamzeh <S.Hamzeh@nitro.local>
2025-04-29 16:34:18 +0330

    center main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
   return (
     <QueryClientProvider client={queryClient}>
-    <main className="bg-[url('/images/hero_img.jpg')]  w-full h-screen bg-no-repeat bg-cover grid grid-cols-2 gap-x-8 m-auto p-12 items-center">
+    <main className="bg-[url('/images/hero_img.jpg')]  w-full h-screen bg-no-repeat bg-cover grid grid-cols-2  m-auto  items-center justify-items-center">
       <Card/>
       <AnswerCard/>
     </main>
